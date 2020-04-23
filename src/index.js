25f1a129d8f4aab6405816d4a2d0cc64ad87036e
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-04-23 20:20:03 +0430

    add Demo component to deiplay home pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from "./serviceWorker";
-import './index.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route  } from 'react-router-dom'
+import * as serviceWorker from "./serviceWorker"
+
+// Scenes
+import Demo from './scenes/Demo'
+
+// Stylesheet
+import './index.scss'
 
 const Root = () => {
     return (
-        <div>
-            <h1>Welcome!</h1>
-        </div>
+        <BrowserRouter basename={'/'}>
+            <Switch>
+                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Demo}/>
+            </Switch>
+        </BrowserRouter>
     )
 }
 
